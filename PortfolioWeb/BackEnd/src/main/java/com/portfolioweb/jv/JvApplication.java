1296b2a5bf18778742cb720c3e541902a8ee2dfa
JulioCV82 <jcv82@outlook.com.ar>
2023-03-01 19:46:48 -0300

    Comienzo spring backend

--- PortfolioWeb/BackEnd/src/main/java/com/portfolioweb/jv/JvApplication.java
+++ PortfolioWeb/BackEnd/src/main/java/com/portfolioweb/jv/JvApplication.java
@@ -1,4 +1,4 @@
-package com.portfolio.jv;
+package com.portfolioweb.jv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- PortfolioWeb/BackEnd/src/test/java/com/portfolioweb/jv/JvApplicationTests.java
+++ PortfolioWeb/BackEnd/src/test/java/com/portfolioweb/jv/JvApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portfolio.jv;
+package com.portfolioweb.jv;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
